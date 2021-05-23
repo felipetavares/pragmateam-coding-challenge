18b1b3efd80b1c78569217bd2fe29e1ff24e62ff
Felipe Tavares <felipe.oltavares@gmail.com>
2021-05-23 18:13:27 -0300

    Added a Header component

---
+++ client/src/components/Header.test.js
@@ -0,0 +1,32 @@
+import '@testing-library/jest-dom/extend-expect'
+import {render, cleanup} from '@testing-library/react';
+import Header from './Header';
+
+// Used to test components with random (but constant) data
+const randomString = 'CI91VapE8M';
+const randomStrings = [
+    'CI91VapE8M', '9vumGrCCHV', '0iZhkVzxXc',
+    'yqinKY6YwK', 'tsvzpwAS2y', 'BB6rBCkRbd',
+    '8K7I2iwh6K', 'taWUtMwQAO', '2xY5BobHAh',
+];
+
+describe('<Header />', () => {
+    it('renders title property', () => {
+        const titles = randomStrings;
+
+        for (const title of titles) {
+            const {getByRole} = render(<Header title={title} />);
+            expect(getByRole('heading')).toHaveTextContent(title);
+            cleanup();
+        }
+    });
+
+    it('uses semantic components', () => {
+        const title = randomString;
+
+        const {getByRole} = render(<Header title={title} />);
+
+        expect(getByRole('banner')).toBeTruthy();
+    })
+});
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <header>
-      <h1>SensorTech</h1>
-    </header>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
